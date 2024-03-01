d8be53b3efbc1c9aa43c679aa627450d938d29c5
Udhayakumar Govindarajan <usuriya530@gmail.com>
2024-03-01 14:47:33 +0530

    logo change and minor text changes in hero page

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -15,11 +15,13 @@ const Hero = () => {
             whileInView="visible"
             viewport={{ once: false }}
             className="flex flex-col justify-center px-8 pt-8 my-auto md:justify-start md:pt-0 md:px-24 lg:px-32">
-                        <img src={logo} alt="logo" className="flex items-center justify-center my-8 w-72 mr-2 full " />
-                        <h1 className="text-6xl text-sky-600  leading-relaxed text-start">
-                            Education Opens Up The Mind
+                        <img src={logo} alt="logo" className="flex items-center justify-center my-8 mr-2 w-72 full " />
+                        <h1 className="text-6xl leading-relaxed text-sky-600 text-start">
+                            Education <span
+                className="text-transparent bg-clip-text bg-gradient-to-br from-blue-200 from-20% via-blue-400 via-30% to-blue-800">Opens</span> Up The <span
+                className="text-transparent bg-clip-text bg-gradient-to-br from-blue-200 from-20% via-blue-400 via-30% to-blue-800">Mind</span>
                         </h1>
-                        <div className="pt-12 pb-12 text-2xl font-light  text-start">
+                        <div className="pt-12 pb-12 text-2xl font-light text-start">
                             <h4>At Eduprajna learn tomorrow technologies today from industry experts. Empowering students with knowledge and skills for the future.</h4>
                         </div>
                     </motion.div>
